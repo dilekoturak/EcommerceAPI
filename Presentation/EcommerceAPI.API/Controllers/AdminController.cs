58534ac775a44adeea13b838ad175168c80c3d7e
Dilek <dilekotrk@hotmail.com>
2022-10-13 00:04:07 -0700

    some test service for admin controller

---
+++ Presentation/EcommerceAPI.API/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EcommerceAPI.Application.Repositories;
+using EcommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+
+        public AdminController(IProductWriteRepository productWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+        }
+
+        [HttpPost("AddProduct/product")]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Id = Guid.NewGuid(),
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock
+            });
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+    }
+}
+
